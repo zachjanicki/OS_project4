fd5abbb8479210802b915f4f2546882f5fa10196
Zach Janicki <zjanicki88@gmail.com>
2017-03-20 21:28:03 -0400

    updated readConfigFile.cpp and added examples for libcurl

--- readConfigFile.cpp
+++ readConfigFile.cpp
@@ -31,11 +31,38 @@ public:
       stringstream tempStream(linesVec[i]);
       vector<string> paramAndValue;
       string temp;
+      int badParamFlag = 0;
       while (getline(tempStream, temp, '=')) {
+        if (paramAndValue.size() == 0 && (temp != "PERIOD_FETCH" ||
+                                        temp != "NUM_FETCH" ||
+                                        temp != "NUM_PARSE" ||
+                                        temp != "SEARCH_FILE" ||
+                                        temp != "SITE_FILE")) {
+          badParamFlag = 1;
+        }
         paramAndValue.push_back(temp);
       }
-      params.push_back(paramAndValue[0]);
-      values.push_back(paramAndValue[1]);
+      if (!badParamFlag) {
+        params.push_back(paramAndValue[0]);
+        values.push_back(paramAndValue[1]);
+      } else {
+        cout << "Illegal parameter '" << paramAndValue[0] << "' given" << endl;
+      }
+    }
+    // check to make sure SEARCH_FILE and SITE_FILE are given
+    int searchFileFlag = 0;
+    int siteFileFlag = 0;
+    for (int i = 0; i < params.size(); i++) {
+      if (params[i] == "SEARCH_FILE") {
+        searchFileFlag = 1;
+      }
+      if (params[i] == "SITE_FILE") {
+        siteFileFlag = 1;
+      }
+    }
+    if (!searchFileFlag || !siteFileFlag) {
+      cout << "missing either SEARCH_FILE or SITE_FILE parameter" << endl;
+      exit(0);
     }
   }
 
